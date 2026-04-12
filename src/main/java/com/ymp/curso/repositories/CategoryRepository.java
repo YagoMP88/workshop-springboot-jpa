bb93b0e080ce4d67c80fbf1ffa9f448ca0b72f5a
YagoMP88 <yagomartins013@gmail.com>
2026-04-12 17:38:44 -0300

    Category

---
+++ src/main/java/com/ymp/curso/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.ymp.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ymp.curso.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+}
